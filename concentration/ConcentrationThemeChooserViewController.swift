5a6f1eec8a29151f89fd332c0f94965d6f9dc773
Lambo <lambo9251@gmail.com>
2018-01-31 21:30:46 -0500

    Jan 31 21:30

--- concentration/ConcentrationThemeChooserViewController.swift
+++ concentration/ConcentrationThemeChooserViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ConcentrationThemeChooserViewController: UIViewController {
+class ConcentrationThemeChooserViewController: UIViewController, UISplitViewControllerDelegate {
 
     let themes = [
         "Emoji":"👻🎃🤖🤯🧟‍♂️🧛🏻‍♂️🦇🕸",
@@ -17,23 +17,47 @@ class ConcentrationThemeChooserViewController: UIViewController {
         "Foods":"🥐🍔🍕🥪🍣🌭🍟🍜",
     ]
     
-//    @IBAction func changeTheme(_ sender: Any) {
-//        if let cvc = splitViewDetailConcentrationViewController {
-//            if let theneName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
-//                cvc.theme = theme
-//            }
-//        } else {
-//        performSegue(withIdentifier: "Choose Theme", sender: sender)
-//        }
-//    }
-//    
-//    private var splitViewDetailConcentrationViewController: ConcentrationViewController? {
-//        return splitViewController?.viewControllers.last as? ConcentrationViewController
-//    }
+    override func awakeFromNib() {
+        splitViewController?.delegate = self
+    }
+    
+    func splitViewController(
+        _ splitViewController: UISplitViewController,
+        collapseSecondary secondaryViewController: UIViewController,   //detail
+        onto primaryViewController: UIViewController     //master
+        ) -> Bool {
+        if let cvc = secondaryViewController as? ConcentrationViewController {
+            if cvc.theme == nil {
+                return true
+            }
+        }
+        return false
+    }
+    
+    @IBAction func changeTheme(_ sender: Any) {
+        if let cvc = splitViewDetailConcentrationViewController {
+            if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
+                cvc.theme = theme
+            }
+        } else if let cvc = lastSeguedToConcentrationViewController {
+            if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
+                cvc.theme = theme
+            }
+            navigationController?.pushViewController(cvc, animated: true)
+        } else {
+        performSegue(withIdentifier: "Choose Theme", sender: sender)
+        }
+    }
+    
+    private var splitViewDetailConcentrationViewController: ConcentrationViewController? {
+        return splitViewController?.viewControllers.last as? ConcentrationViewController
+    }
     
     
     
     // MARK: - Navigation
+    
+    private var lastSeguedToConcentrationViewController: ConcentrationViewController?
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "Choose Theme" {
@@ -42,6 +66,7 @@ class ConcentrationThemeChooserViewController: UIViewController {
             if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
                 if let cvc = segue.destination as? ConcentrationViewController {
                     cvc.theme = theme
+                    lastSeguedToConcentrationViewController = cvc
                 }
             }
         }
